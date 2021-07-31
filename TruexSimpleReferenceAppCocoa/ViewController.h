ad5b335a7c4bf017db138dc5da7b1175362752b5
Isaiah Mann <isaiah@truex.com>
2021-07-30 19:40:25 -0700

    Track reference app with CocoaPods deps

---
+++ TruexSimpleReferenceAppCocoa/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TruexSimpleReferenceApp
+//
+//  Copyright © 2021 true[X]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TruexSimpleReferenceAppCocoa/ViewController.m
@@ -0,0 +1,115 @@
+//
+//  ViewController.m
+//  TruexSimpleReferenceApp
+//
+//  Copyright © 2021 true[X]. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@import AVKit;
+@import TruexAdRenderer;
+
+@interface ViewController () <TruexAdRendererDelegate>
+
+@property AVPlayer *player;
+@property AVPlayerViewController *playerController;
+@property TruexAdRenderer *adRenderer;
+@property NSString *currentAdSlotType;
+
+@end
+
+@implementation ViewController
+
+static NSString* const StreamURLString = @"https://ctv.truex.com/assets/reference-app-stream-no-cards-1080p.mp4";
+
+// business logic constants:
+static int const AdBreakEndSeconds = 93;
+static int const MidrollAdBreakDimensionValue = 2;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.playerController = [AVPlayerViewController new];
+    NSURL* streamUrl = [NSURL URLWithString:StreamURLString];
+    self.player = [AVPlayer playerWithURL:streamUrl];
+    self.playerController.player = self.player;
+    [self.view addSubview:self.playerController.view];
+    self.playerController.view.frame = self.view.frame;
+
+    [self setAdBreaks:self.player];
+
+    self.adRenderer = [self initializeAdRenderer:MIDROLL];
+}
+
+- (void)setAdBreaks:(AVPlayer *)player {
+    AVPlayerItem *content = player.currentItem;
+    content.interstitialTimeRanges = @[
+        // time: 0:00
+        [[AVInterstitialTimeRange alloc] initWithTimeRange:CMTimeRangeMake(kCMTimeZero, CMTimeMake(90, 1))],
+        // time: 9:52
+        [[AVInterstitialTimeRange alloc] initWithTimeRange:CMTimeRangeMake(CMTimeMake(592, 1), CMTimeMake(90, 1))]
+    ];
+}
+
+- (TruexAdRenderer*)initializeAdRenderer:(NSString *)adSlotType {
+    TruexAdRenderer *renderer = [[TruexAdRenderer alloc] initWithUrl:@""
+                                                        adParameters:[self getFakeAdParams]
+                                                            slotType:adSlotType];
+    self.currentAdSlotType = adSlotType;
+    renderer.delegate = self;
+    return renderer;
+}
+
+// Fake response from an ad server
+- (NSDictionary*)getFakeAdParams {
+    // workaround to always show the ad (simulates a different user each time):
+    NSString *userId = [NSUUID UUID].UUIDString;
+    
+    // final string should format to (network_user_id parameter will change value each time): qa-get.truex.com/bdfe2ba97e74172a75e325d307db6cfc16f92325/vast/config?asnw=&cpx_url=&dimension_2=2&stream_prosition=midroll&flag=%2Bamcb%2Bemcr%2Bslcb%2Bvicb%2Baeti-exvt&fw_key_values=&metr=0&network_user_id=93DDE21A-6F37-425D-A6E0-671329EE6A01&prof=g_as3_truex&ptgt=a&pvrn=&resp=vmap1&slid=fw_truex&ssnw=&stream_id=136083572&vdur=&vprn=
+    NSString *vastConfigUrl = [NSString stringWithFormat:@"qa-get.truex.com/bdfe2ba97e74172a75e325d307db6cfc16f92325/vast/config?asnw=&cpx_url=&dimension_2=%d&stream_prosition=%@&flag=%%2Bamcb%%2Bemcr%%2Bslcb%%2Bvicb%%2Baeti-exvt&fw_key_values=&metr=0&network_user_id=%@&prof=g_as3_truex&ptgt=a&pvrn=&resp=vmap1&slid=fw_truex&ssnw=&stream_id=136083572&vdur=&vprn=",
+                               MidrollAdBreakDimensionValue,
+                               MIDROLL,
+                               userId];
+    NSLog(@"[TRUEX DEBUG] requesting ad from Vast Config URL: %@", vastConfigUrl);
+    
+    // TODO: make this configurable outside the source code
+    return @{
+          @"placement_hash" : @"bdfe2ba97e74172a75e325d307db6cfc16f92325",
+          @"vast_config_url" : vastConfigUrl,
+          @"user_id" : userId
+    };
+}
+
+// MARK: - true[X] Ad Renderer Delegate Methods
+-(void) onAdStarted:(NSString*)campaignName {
+    NSLog(@"Showing ad: %@", campaignName);
+}
+
+-(void) onAdFreePod {
+    CMTime seekTime = CMTimeMakeWithSeconds(AdBreakEndSeconds, 1000);
+    [self.player seekToTime:seekTime toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero];
+    // TODO: add logic to handle skip linear ads not at start of video
+}
+
+-(void) onAdCompleted:(NSInteger)timeSpent {
+    [self.player play];
+}
+
+-(void) onAdError:(NSString *)errorMessage {
+    [self.player play];
+}
+
+-(void) onNoAdsAvailable {
+    [self.player play];
+}
+
+-(void) onUserCancelStream {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void) onFetchAdComplete {
+    [self.adRenderer start:self.playerController];
+}
+
+@end
